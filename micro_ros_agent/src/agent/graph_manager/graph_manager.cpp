d9fe8c6aab153df672f1bdfea7b6bb957e7f196e
Jan Hernas <hernasj@wp.pl>
2025-07-30 11:47:19 +0200

    Fix node namespacing

--- micro_ros_agent/src/agent/graph_manager/graph_manager.cpp
+++ micro_ros_agent/src/agent/graph_manager/graph_manager.cpp
@@ -596,10 +596,9 @@ void GraphManager::get_name_and_namespace(
         node_namespace = "/";
     }
 
-    // Apply namespace prefix if configured
     if (!namespace_prefix_.empty() && namespace_prefix_ != "/")
     {
-        node_namespace = utils::Namespace::apply_namespace_to_node(node_namespace, namespace_prefix_);
+        node_namespace = namespace_prefix_ + node_namespace;
     }
 }
 
